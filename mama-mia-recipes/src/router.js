84a0611e2074647cc5e8dc955d7f79c8a48b9bdf
huntleyfrancesta <huntleyfrancesta@gmail.com>
2021-06-11 23:01:10 -0400

    commit

--- mama-mia-recipes/src/router.js
+++ mama-mia-recipes/src/router.js
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useContext } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-
+import AuthHome from "./auth/AuthHome";
+import UserContext from "./context/UserContext";
+import EditRecipe from "./recipe/EditRecipe";
+import NewRecipe from "./recipe/NewRecipe";
+import Recipe from "./recipe/Recipe";
+import RecipeList from "./recipe/RecipeList";
+import RecipeNavbar from "./recipe/RecipeNavbar";
 
 const Router = () => {
-    return (
+  const user = useContext(UserContext);
 
-     <BrowserRouter>
-    <Switch>
-        <Route exat path="/">
-            Home
-        </Route>
-        </Switch>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Switch>
+        {user ? (
+          <>
+            <RecipeNavbar />
+            <Route exact path="/">
+              <RecipeList />
+            </Route>
+            <Route path="/new">
+              <NewRecipe />
+            </Route>
+            <Route path="/recipe/:id" component={Recipe} />
+            <Route path="/edit/:id" component={EditRecipe} />
+          </>
+        ) : (
+          <>
+            <AuthHome />
+          </>
+        )}
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 export default Router;
